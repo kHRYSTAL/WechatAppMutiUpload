6a86a50f1bdf9e6fc9a226b97eae718192dc6a09
kHRYSTAL <723526676@qq.com>
2017-11-04 23:30:13 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,31 +4,31 @@ var extend = require('../../lib/extend.js');
 
 const app = getApp()
 var pageConf = {
-  data: {
-    imageUrl: ''
-  },
+    data: {
+        imageUrl: ''
+    },
 
-  bindViewTap: function (e) {
-    var that = this;
-    that.chooseImageUpload({
-      maxCount: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      remote: 'https://192.168.1.125:8001/upload/image/',
-      callback: function (res) {
-        console.log("get upload response:", res);
-        if (res.success) { // example {'success': true, 'index': 0, imgUrl:'url'}
-          console.log("set image url", res.imgUrl);
-          that.setData({
-            imageUrl: res.imgUrl
-          });
-        }
-      },
-      complete: function (res) {
-        console.log(res);
-      }
-    });
-  }
+    bindViewTap: function (e) {
+        var that = this;
+        that.chooseImageUpload({
+            maxCount: 1,
+            sizeType: ['compressed'],
+            sourceType: ['album', 'camera'],
+            remote: 'https://192.168.1.125:8001/upload/image/',
+            callback: function (res) {
+                console.log("get upload response:", res);
+                if (res.success) { // example {'success': true, 'index': 0, imgUrl:'url'}
+                    console.log("set image url", res.imgUrl);
+                    that.setData({
+                        imageUrl: res.imgUrl
+                    });
+                }
+            },
+            complete: function (res) {
+                console.log(res);
+            }
+        });
+    }
 }
 
 // deep clone, notice the second argument is pageConf
